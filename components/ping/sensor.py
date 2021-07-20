251a0d5d62c9c049c4266e3b28e3b6b8f23d389b
Tomoyuki Sakurai <y@trombik.org>
2021-07-21 00:59:42 +0700

    bugfix: lint with black

--- components/ping/sensor.py
+++ components/ping/sensor.py
@@ -17,9 +17,8 @@ DEPENDENCIES = ["network"]
 
 ping_ns = cg.esphome_ns.namespace("ping")
 
-PingSensor = ping_ns.class_(
-    "PingSensor", sensor.Sensor, cg.PollingComponent
-)
+PingSensor = ping_ns.class_("PingSensor", sensor.Sensor, cg.PollingComponent)
+
 
 def validate_timeout(n):
     n = cv.positive_time_period_microseconds(n)
@@ -27,33 +26,33 @@ def validate_timeout(n):
         raise cv.Invalid("Maximum timeout cannot be greater than 8 seconds")
     return n
 
+
 CONF_LOSS = "loss"
 CONF_LATENCY = "latency"
 
-CONFIG_SCHEMA = (
-        cv.Schema(
-            {
-                cv.GenerateID(): cv.declare_id(PingSensor),
-                cv.Optional(CONF_IP_ADDRESS, default="8.8.8.8"): cv.string,
-                cv.Optional(CONF_TIMEOUT, default="1sec"): cv.positive_time_period_milliseconds,
-                cv.Optional(CONF_NUM_ATTEMPTS, default=13): cv.int_range(min=1, max=60),
-                cv.Optional(CONF_LOSS): sensor.sensor_schema(
-                    UNIT_PERCENT,
-                    ICON_EMPTY,
-                    0,
-                    DEVICE_CLASS_EMPTY,
-                    STATE_CLASS_MEASUREMENT,
-                ),
-                cv.Optional(CONF_LATENCY): sensor.sensor_schema(
-                    UNIT_SECOND,
-                    ICON_EMPTY,
-                    0,
-                    DEVICE_CLASS_EMPTY,
-                    STATE_CLASS_MEASUREMENT,
-                ),
-            }
-        ).extend(cv.polling_component_schema("60s"))
-    )
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(PingSensor),
+        cv.Optional(CONF_IP_ADDRESS, default="8.8.8.8"): cv.string,
+        cv.Optional(CONF_TIMEOUT, default="1sec"): cv.positive_time_period_milliseconds,
+        cv.Optional(CONF_NUM_ATTEMPTS, default=13): cv.int_range(min=1, max=60),
+        cv.Optional(CONF_LOSS): sensor.sensor_schema(
+            UNIT_PERCENT,
+            ICON_EMPTY,
+            0,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_LATENCY): sensor.sensor_schema(
+            UNIT_SECOND,
+            ICON_EMPTY,
+            0,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_MEASUREMENT,
+        ),
+    }
+).extend(cv.polling_component_schema("60s"))
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
